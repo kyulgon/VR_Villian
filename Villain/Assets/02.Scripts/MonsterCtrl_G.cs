66159cd8be498b76e0de69076b3be10e44ec0a5c
kyulgon <57319096+kyulgon@users.noreply.github.com>
2021-06-10 22:39:34 +0900

    머신 체력수정 전

--- Villain/Assets/02.Scripts/MonsterCtrl_G.cs
+++ Villain/Assets/02.Scripts/MonsterCtrl_G.cs
@@ -33,9 +33,8 @@ public class MonsterCtrl_G : MonoBehaviour
     public Slider hpSlider;
 
 
-    public float timeBetFire = 0.12f; // 탄알 발사 간격
-    public float reloadTime = 1.8f; // 재장전 소요 시간
-    private float lastFireTime; // 총을 마지막으로 발사한 시점(연사 구현시 사용)
+    public float timeBetFire = 3f; // 몬스터 공각 대기 시간
+    private float lastFireTime; // 보스 공격을 마지막으로 발사한 시점
 
 
     public AudioSource attackClip;
@@ -46,135 +45,143 @@ public class MonsterCtrl_G : MonoBehaviour
     {
         monsterTr = this.gameObject.GetComponent<Transform>();
         playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        
         nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        // nvAgent.destination = playerTr.position;
+        nvAgent.destination = playerTr.position;
 
-        StartCoroutine(this.CheckMonsterState());
+        //StartCoroutine(this.CheckMonsterState());
         
 
-        attackAudio = GetComponent<AudioSource>();
+        //attackAudio = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         //hpSlider.value = hp / maxhp;// 슬라이더 
-        //nvAgent.destination = playerTr.position;
-        //animator.SetBool("IsTrace", true);
+        nvAgent.destination = playerTr.position;
+        animator.SetBool("IsTrace", true);
 
         hpSlider.value = hp / maxhp;
     }
 
     public void Fire()
     {
-        if (monsterState == MonsterState.attack && Time.time >= lastFireTime + timeBetFire) // 현재상태가 발사 가능한 상태 && 마지막 총 발사 시점에서 발사간격 이상의 시간이 지남
-        {
-            lastFireTime = Time.time; // 마지막 총 발사 시점 갱신
-            StartCoroutine(this.MonsterAction());
-        }
+        
+        //if (monsterState == MonsterState.attack && Time.time >= lastFireTime + timeBetFire) // 현재상태가 발사 가능한 상태 && 마지막 총 발사 시점에서 발사간격 이상의 시간이 지남
+        //{
+        //    Debug.Log("fire");
+        //    lastFireTime = Time.time; // 마지막 총 발사 시점 갱신
+        //    StartCoroutine(this.MonsterAction());
+        //}
     }
 
-    public void GetDamage(float amount)
-    {
-        hp -= (int) (amount);
-        hpSlider.value = hp;
+    //public void GetDamage(float amount) // 데미지 받기
+    //{
+    //    hp -= (int) (amount);
+    //    hpSlider.value = hp;
 
-        if (hp <= 0)
-        {
-            MonsterDie();
-        }
-    }
+    //    if (hp <= 0)
+    //    {
+    //        MonsterDie();
+    //    }
+    //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "BossMonster")
-        {
-            MonsterCtrl_G boss = other.GetComponent<MonsterCtrl_G>();
+    //private void OnTriggerEnter(Collider other) // bossMonster 충돌
+    //{
+    //    if(other.tag == "BossMonster")
+    //    {
+    //        Debug.Log("트리거");
+    //        MonsterCtrl_G boss = other.GetComponent<MonsterCtrl_G>();
 
-            if(boss != null)
-            {
-                boss.GetDamage(attackAmount);
-            }
-        }
-    }
+    //        if(boss != null)
+    //        {
+    //            boss.GetDamage(attackAmount);
+    //        }
+    //    }
+    //}
 
-    IEnumerator CheckMonsterState()
-    {
-        while(!isDie)
-        {
-            yield return new WaitForSeconds(0.2f);
+    //IEnumerator CheckMonsterState() // 몬스터 상태 바꾸기
+    //{
+    //    while(!isDie)
+    //    {
+    //        yield return new WaitForSeconds(0.2f);
 
-            float dist = Vector3.Distance(playerTr.position, monsterTr.position);
+    //        float dist = Vector3.Distance(playerTr.position, monsterTr.position);
 
-            if(dist <= attackDist)
-            {
-                monsterState = MonsterState.attack;
-            }
-            else if(dist <= traceDist)
-            {
-                monsterState = MonsterState.trace;
-            }
-            else
-            {
-                monsterState = MonsterState.idle;
-            }
-        }
-    }
+    //        if(dist <= attackDist)
+    //        {
+    //            Debug.Log("어택상태");
+    //            monsterState = MonsterState.attack;
+    //        }
+    //        else if(dist <= traceDist)
+    //        {
+    //            Debug.Log("추적상태");
+    //            monsterState = MonsterState.trace;
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("멈춤상태");
+    //            monsterState = MonsterState.idle;
+    //        }
+    //    }
+    //}
 
-    IEnumerator MonsterAction()
-    {
-        while(!isDie)
-        {
-            switch (monsterState)
-            {
-                case MonsterState.idle:
-                    nvAgent.isStopped = true;
-                    animator.SetBool("IsTrace", false);
-                    break;
+    //IEnumerator MonsterAction() // 상태에 따른 몬스터 액션
+    //{
+    //    while(!isDie)
+    //    {
+    //        switch (monsterState)
+    //        {
+    //            case MonsterState.idle:
+    //                nvAgent.isStopped = true;
+    //                animator.SetBool("IsTrace", false);
+    //                break;
 
-                case MonsterState.trace:
-                    nvAgent.destination = playerTr.position;
-                    nvAgent.isStopped = false;
-                    animator.SetBool("IsAttack", false);
-                    animator.SetBool("IsTrace", true);
-                    break;
+    //            case MonsterState.trace:
+    //                nvAgent.destination = playerTr.position;
+    //                nvAgent.isStopped = false;
+    //                Debug.Log("Trace");
+    //                animator.SetBool("IsAttack", false);
+    //                animator.SetBool("IsTrace", true);
+    //                break;
 
-                case MonsterState.attack:
-                    nvAgent.isStopped = true;
-                    StartCoroutine(AttackEffect());
-                    break;
-            }
-            yield return null;
-        }
-    }
+    //            case MonsterState.attack:
+    //                nvAgent.isStopped = true;
+    //                StartCoroutine(AttackEffect());
+    //                break;
+    //        }
+    //        yield return null;
+    //    }
+    //}
 
-    IEnumerator AttackEffect()
-    {
-        animator.SetBool("IsAttack", true);
-        bossAttackEffect.Play();
+    //IEnumerator AttackEffect() // 공격 이펙트
+    //{
+    //    animator.SetBool("IsAttack", true);
+    //    bossAttackEffect.Play();
 
-        yield return new WaitForSeconds(3.0f);
-    }
+    //    yield return new WaitForSeconds(3.0f);
+    //}
 
-    void MonsterDie()
-    {
-        if (isDie == true)
-            return;
+    //void MonsterDie() // 몬스터 다이
+    //{
+    //    if (isDie == true)
+    //        return;
 
-        StopAllCoroutines();
-        isDie = true;
-        monsterState = MonsterState.die;
-        nvAgent.isStopped = true;
-        animator.SetBool("IsDie", true);
+    //    StopAllCoroutines();
+    //    isDie = true;
+    //    monsterState = MonsterState.die;
+    //    nvAgent.isStopped = true;
+    //    animator.SetBool("IsDie", true);
 
-        // FindObjectOfType<MenuUI_G>().BossDie();
-        gameObject.GetComponentInChildren<CapsuleCollider>().enabled = false;
-        foreach (Collider coll in gameObject.GetComponentsInChildren<SphereCollider>())
-        {
-            coll.enabled = false;
-        }
-        Destroy(this.gameObject, 3f); ;
-    }
+    //    // FindObjectOfType<MenuUI_G>().BossDie();
+    //    gameObject.GetComponentInChildren<CapsuleCollider>().enabled = false;
+    //    foreach (Collider coll in gameObject.GetComponentsInChildren<SphereCollider>())
+    //    {
+    //        coll.enabled = false;
+    //    }
+    //    Destroy(this.gameObject, 3f); ;
+    //}
 
 
     
